74dc3682ed0955c1e8f0b221bf80b28c4ded2a4d
Lee Si Woong <silverttthin@naver.com>
2025-03-30 21:32:48 +0900

    feat: message 엔티티 생성자 추가 및 user 추가 - 메시지와 유저는 다대일 매핑(추후 uuid 변경 논의대상) - 빌더 생성자 추가 - 메시지 변경 메서드 추가(다른 필드는 로직상 불필요하다 판단)

--- src/main/java/org/glue/glue_be/chat/entity/Message.java
+++ src/main/java/org/glue/glue_be/chat/entity/Message.java
@@ -3,15 +3,15 @@ package org.glue.glue_be.chat.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.glue.glue_be.common.BaseEntity;
+import org.glue.glue_be.user.entity.User;
 
 
 @Entity
 @Getter
-@Builder
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "message")
-public class Message {
+public class Message extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,7 +24,21 @@ public class Message {
 	@JoinColumn(name = "chatroom_id", nullable = false)
 	private ChatRoom chatRoom;
 
+	// 추후 이부분을 일반 속성인 uuid로 대체해야 할지 결정해야함
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
 	@Column(name = "message_content", columnDefinition = "TEXT", nullable = false)
 	private String messageContent;
+
+	@Builder
+	private Message(ChatRoom chatRoom, User user, String messageContent){
+		this.chatRoom = chatRoom;
+		this.user = user;
+		this.messageContent = messageContent;
+	}
+
+	public void changeMessageContent(String newMessageContent) { this.messageContent = newMessageContent; }
 
 }
