f01a1611504e602148a2710f595df7bcc96e52db
Lee Si Woong <silverttthin@naver.com>
2025-03-30 13:54:46 +0900

    feat: 채팅방 생성자 생성 - 기존 Builder를 새로 만든 생성자에 붙임 - 이 생성자는 pk는 수정할 수 없도록 함

--- src/main/java/org/glue/glue_be/chat/entity/ChatRoom.java
+++ src/main/java/org/glue/glue_be/chat/entity/ChatRoom.java
@@ -7,8 +7,6 @@ import org.glue.glue_be.meeting.entity.Meeting;
 
 @Entity
 @Getter
-@Builder(toBuilder = true)
-@AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "chatroom")
 public class ChatRoom {
@@ -21,7 +19,14 @@ public class ChatRoom {
 	// chatroom이 비즈니스적으로 meeting에 속하기 때문에 여기가 FK를 가짐
 	// cascade는 meeting과의 관계에 대해 명확히 알게되면 추가해야 할 듯
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "meeting_id", unique = true, nullable = false)
+	@JoinColumn(name = "meeting_id", nullable = false)
 	private Meeting meeting;
+
+	// 생성자를 따로 만들고 Builder를 붙임 -> Allargs 배제
+	@Builder
+	public ChatRoom(Meeting meeting){
+		this.meeting = meeting;
+	}
+
 
 }
