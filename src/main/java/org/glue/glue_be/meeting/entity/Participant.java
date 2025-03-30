c95e2f0e37c0d19452b2bf78fac3a74a3383d568
kyleidea1 <99781357+kyleidea1@users.noreply.github.com>
2025-03-30 14:54:06 +0900

    Revert "refactor: modified Participant.java Entity Class"
    This reverts commit 7143dfae319a4c03cbd954cb1501a517ecbea87c.

--- src/main/java/org/glue/glue_be/meeting/entity/Participant.java
+++ src/main/java/org/glue/glue_be/meeting/entity/Participant.java
@@ -5,7 +5,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.glue.glue_be.user.entity.User;
-import lombok.Builder;
 
 @Entity
 @Table(name = "participant")
@@ -18,31 +17,35 @@ public class Participant {
     @Column(name = "participant_id", updatable = false)
     private Long id;
 
+    // Many Participants → One User
+    // User에서는 OneToMany(mappedBy = "user")로써 Participant.user 필드를 기준으로 연관관계가 매핑됨을 표현해야 함
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    // Many Participants → One Meeting
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "meeting_id", nullable = false)
     private Meeting meeting;
 
-    @Builder
-    private Participant(User user, Meeting meeting) {
-        this.user = user;
+    void setMeeting(Meeting meeting) {
         this.meeting = meeting;
     }
 
-    void updateMeeting(Meeting meeting) {
-        this.meeting = meeting;
-    }
-
-    void updateUser(User user) {
+    void setUser(User user) {
         this.user = user;
     }
 
-    public void cancelParticipation() {
-        if (this.meeting != null) {
-            this.meeting.removeParticipant(this);
-        }
-    }
+    // 양방향 정합성을 위해
+    //    public void addParticipant(Participant participant) {
+    //        participants.add(participant);
+    //        participant.setUser(this);
+    //    }
+    //
+    //    public void addParticipant(Participant participant) {
+    //        participants.add(participant);
+    //        participant.setMeeting(this);
+    //    }
+    // 를 User, Meeting 측에 추가하면 좋을 듯
+
 }
