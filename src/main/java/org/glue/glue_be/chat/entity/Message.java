077c6e885e927e753f330d9fe6e1cdc699523196
Lee Si Woong <silverttthin@naver.com>
2025-03-30 14:07:30 +0900

    comment: message의 user 연관관계 주석 추가

--- src/main/java/org/glue/glue_be/chat/entity/Message.java
+++ src/main/java/org/glue/glue_be/chat/entity/Message.java
@@ -18,6 +18,8 @@ public class Message {
 	@Column(name = "message_id")
 	private Long id;
 
+	// Message와 User는 매개 테이블 없이 직접적인 연관관계를 매핑
+	// 따라서 반대측인 User에서 OneToMany를 작성할 필요 없음
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "chatroom_id", nullable = false)
 	private ChatRoom chatRoom;
