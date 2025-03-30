0a3113904c8fa0f618ea240299124911cb2dbe57
Lee Si Woong <silverttthin@naver.com>
2025-03-30 22:03:54 +0900

    feat: userChatroom 메서드 추가 - 생성자 및 수정 메서드 추가

--- src/main/java/org/glue/glue_be/chat/entity/UserChatroom.java
+++ src/main/java/org/glue/glue_be/chat/entity/UserChatroom.java
@@ -3,16 +3,15 @@ package org.glue.glue_be.chat.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.glue.glue_be.common.BaseEntity;
 import org.glue.glue_be.user.entity.User;
 
 
 @Entity
 @Getter
-@Builder
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "user_chatroom")
-public class UserChatroom {
+public class UserChatroom extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,5 +25,17 @@ public class UserChatroom {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "chatRoom_id", nullable = false)
 	private ChatRoom chatRoom;
+
+	@Builder
+	private UserChatroom(User user, ChatRoom chatRoom){
+		this.user = user;
+		this.chatRoom = chatRoom;
+	}
+
+	void updateUser(User user){ this.user = user; }
+
+	void updateChatRoom(ChatRoom chatRoom) { this.chatRoom = chatRoom; }
+
+
 
 }
