93d270f91a8beff35378c19dffcc465e58ad2189
Lee Si Woong <silverttthin@naver.com>
2025-04-10 14:57:23 +0900

    feat: 카카오 측 서버 자체 통신에 필요한 서비스와 응답 dto - 회원가입 시 카카오로부터 소셜로그인 자체 pk를 받아와 유저가 있는지 검증하는 용도 - 추후 우렁각시 라이브러리에서 oauthID도 같이 보내게 된다면 생략할 수 도 있음. 이 부분은 논의해야

---
+++ src/main/java/org/glue/glue_be/user/dto/response/KakaoUserInfoResponseDto.java
@@ -0,0 +1,28 @@
+package org.glue.glue_be.user.dto.response;
+
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record KakaoUserInfoResponseDto(
+	Long id,
+	KakaoAccount kakaoAccount
+) {
+
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+	public record KakaoAccount(
+		KakaoUserProfile profile
+	) {
+
+		@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+		public record KakaoUserProfile(
+			String nickname,
+			String profileImageUrl,
+			String accountEmail
+		) {
+		}
+	}
+}
+
