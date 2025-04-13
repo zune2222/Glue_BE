296957cb1f56bfa5c4dac724fe87bfc26e9dd57a
kim-minkyoung <decomin02@pusan.ac.kr>
2025-04-13 15:31:26 +0900

    fix: BaseEntity에 @Getter 추가

--- src/main/java/org/glue/glue_be/common/BaseEntity.java
+++ src/main/java/org/glue/glue_be/common/BaseEntity.java
@@ -2,9 +2,12 @@ package org.glue.glue_be.common;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import lombok.Getter;
 import org.springframework.data.annotation.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@Getter
 @EntityListeners(value = AuditingEntityListener.class)
 @MappedSuperclass
 abstract public class BaseEntity {
